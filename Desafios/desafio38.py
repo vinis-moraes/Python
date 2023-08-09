70a92bbc385a156b80bc463e3de832b3e3176546
Vini Moraes <81641780+vinis-moraes@users.noreply.github.com>
2023-08-09 19:29:12 -0300

    Update desafio38.py

--- Desafios/desafio38.py
+++ Desafios/desafio38.py
@@ -1,5 +1,5 @@
-num1 = int(input('Digite um número: '))
-num2 = int(input('Digite outro número: '))
+num1 = float(input('Digite um número: '))
+num2 = float(input('Digite outro número: '))
 
 if num1 > num2:
     print('O maior número é {}.'.format(num1))
