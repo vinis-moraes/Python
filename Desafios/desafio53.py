6123fcdbb840f0fdcb260fca030b6b0adf103095
Vini Moraes <81641780+vinis-moraes@users.noreply.github.com>
2023-08-24 19:46:19 -0300

    Update desafio53.py

--- Desafios/desafio53.py
+++ Desafios/desafio53.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
 
-frase = str(input("Digite uma frase: "))
+frase = str(input("Digite uma frase: ")).strip().upper()
+palavras = frase.split()
+junto = "".join(palavras)
+inverso = ""
+
+inverso = junto[::-1]
+
+# for letra in range(len(junto) -1, -1, -1):
+#	inverso += junto[letra]
+	
+print(junto, inverso)
+
+if inverso == junto:
+	print("Temos um palíndromo!")
+else:
+	print("Não temos um palíndromo")
