fff097ae5259644badd3cf806e099909dfbe2835
Vini Moraes <81641780+vinis-moraes@users.noreply.github.com>
2023-08-29 19:46:17 -0300

    Update aula14.py

--- Aulas/aula14.py
+++ Aulas/aula14.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
-
 n = 1
+par = impar = 0
 
 while n != 0:
-	n = int(input("Digite um valor: "))
-	
-print("Fim")
+	n = int(input('Digite um valor: '))
+	if n != 0:
+		if n % 2 == 0:
+			par += 1
+		else:
+			impar += 1
+print('Você digitou {} números pares e {} números ímpares.'.format(par, impar))
