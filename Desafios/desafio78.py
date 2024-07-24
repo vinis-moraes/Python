cddf1f8aacf0d07c512879837f592f485b1e74ff
Vini Moraes <81641780+vinis-moraes@users.noreply.github.com>
2024-07-24 13:43:11 -0300

    Create desafio78.py

---
+++ Desafios/desafio78.py
@@ -0,0 +1,23 @@
+list = [
+    int(input('Digite um valor: ')),
+    int(input('Digite outro valor: ')),
+    int(input('Digite mais um valor: ')),
+    int(input('Digite mais um outro valor: ')),
+    int(input('Digite o último valor: '))
+]
+
+menor = [0, 0]
+maior = [0, 0]
+
+
+for c in range(len(list)):
+    if list[c] > maior[0] or maior[0] == 0:
+        maior[0] = list[c]
+        maior[1] = c + 1
+    if list[c] < menor[0] or menor[0] == 0:
+        menor[0] = list[c]
+        menor[1] = c + 1
+
+
+print(f"Menor valor: {menor[0]} na posição {menor[1]}")
+print(f"Maior valor: {maior[0]} na posição {maior[1]}")
