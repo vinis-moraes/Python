222a3f5dc29ff1c56fb284995bcd09b7e3abdff3
Vini Moraes <81641780+vinis-moraes@users.noreply.github.com>
2024-07-23 23:33:56 -0300

    Create desafio73.py

---
+++ Desafios/desafio73.py
@@ -0,0 +1,6 @@
+brasileirao = ("Botafogo", 'Palmeiras', 'Flamengo', 'Fortaleza', 'São Paulo', 'Bahia', 'Cruzeiro', 'Athletico-PR', 'Bragantino', 'Atlético-MG', 'Vasco da Gama', 'Juventude', 'Internacional', 'Corinthians', 'Criciúma', 'Cuiabá', 'Vitória', 'Grêmio', 'Fluminense', "Atlético-GO")
+
+print(f"Os cinco primeiros colocados são: {brasileirao[0:5]}")
+print(f'Os quatro últimos colocados são: {brasileirao[16:20]}')
+print(f'Os times em ordem alfabética são: {sorted(brasileirao)}')
+print("There is no Chapecoense in this championship")
