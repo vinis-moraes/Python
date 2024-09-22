3619b6fa7a3a701b8196121dd55bee81539af394
Vini Moraes <81641780+vinis-moraes@users.noreply.github.com>
2024-09-22 15:19:59 -0300

    Update desafio78.py

--- Desafios/desafio78.py
+++ Desafios/desafio78.py
@@ -12,12 +12,14 @@ maior = [0, 0]
 
 for c in range(len(list)):
     if list[c] > maior[0] or maior[0] == 0:
-        maior[0] = list[c]
-        maior[1] = c + 1
-    if list[c] < menor[0] or menor[0] == 0:
-        menor[0] = list[c]
-        menor[1] = c + 1
+        maior.pop()
+        maior.pop()
+        maior.insert(0, list[c])
+        maior.insert(1, c)
+ #   if list[c] < menor[0] or menor[0] == 0:
+  #      menor[0] = list[c]
+   #     menor[1] = c + 1
 
 
-print(f"Menor valor: {menor[0]} na posição {menor[1]}")
-print(f"Maior valor: {maior[0]} na posição {maior[1]}")
+#print(f"Menor valor: {menor[0]} na posição {menor[1]}")
+print(f"Maior valor: {maior[0]} na(s) posição(ões) {maior[1:]}")
