b2c6197fbb991ba359d07f40a40a48a53b8ecd12
Vini Moraes <81641780+vinis-moraes@users.noreply.github.com>
2024-09-22 15:37:00 -0300

    Update desafio78.py

--- Desafios/desafio78.py
+++ Desafios/desafio78.py
@@ -1,25 +1,21 @@
-list = [
-    int(input('Digite um valor: ')),
-    int(input('Digite outro valor: ')),
-    int(input('Digite mais um valor: ')),
-    int(input('Digite mais um outro valor: ')),
-    int(input('Digite o último valor: '))
-]
+list = []
+for c in range(0,5):
+    list.append(int(input(f'Digite o número para a posição {c}: ')))
+    if c == 0:
+        maior = menor = list[c]
+    else:
+        if list[c] > maior:
+            maior = list[c]
+        if list[c] < menor:
+            menor = list[c]
 
-menor = [0, 0]
-maior = [0, 0]
-
-
-for c in range(len(list)):
-    if list[c] > maior[0] or maior[0] == 0:
-        maior.pop()
-        maior.pop()
-        maior.insert(0, list[c])
-        maior.insert(1, c)
- #   if list[c] < menor[0] or menor[0] == 0:
-  #      menor[0] = list[c]
-   #     menor[1] = c + 1
-
-
-#print(f"Menor valor: {menor[0]} na posição {menor[1]}")
-print(f"Maior valor: {maior[0]} na(s) posição(ões) {maior[1:]}")
+print(f'O maior valor é {maior} na(s) posição(ões) ', end="")
+for i, v in enumerate(list):
+    if v == maior:
+        print(f'{i} ', end="")
+print("")
+print(f'O menor valor é {menor} na(s) posição(ões) ', end="")
+for i, v in enumerate(list):
+    if v == menor:
+        print(f'{i}', end=" ")
+print("")
