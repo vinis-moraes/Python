af61e3271205728a634f758b5b05863f2c0cd40b
Vini Moraes <81641780+vinis-moraes@users.noreply.github.com>
2024-09-22 22:02:48 -0300

    Create desafio80.py

---
+++ Desafios/desafio80.py
@@ -0,0 +1,13 @@
+list = []
+for c in range(0,5):
+    n = input('Digite um valor: ')
+    if c == 0 or n >= list[-1]:
+        list.append(n)
+    else:
+        pos = 0
+        while pos < len(list):
+            if n <= lista[pos]:
+                list.insert(pos, n)
+                break
+            pos += 1
+print(list)
